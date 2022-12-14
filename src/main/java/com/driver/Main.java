83a8beb4cc0ab40bb2aaf1cba453374444b8afc1
Hafeez Basha <shaikhafeez500@gmail.com>
2022-12-14 20:06:23 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,25 @@
 package com.driver;
 
+
 public class Main {
     public static void main(String[] args) {
         B b=new B();
         b.meth();
+
     }
   
 }
+class A{
+    public void meth(){
+        System.out.println("Invoking method from class A");
+    }
+}
+
+class B extends A{
+    @Override
+    public void meth(){
+
+        System.out.println("Method is overridden in Extendend class B");
+    }
+}
+
